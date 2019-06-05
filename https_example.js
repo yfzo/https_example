1b91a9b948cc50be11ceb51a30dfa450ce5fea7e
Yifei <yifeizhou.to@gmail.com>
2019-06-05 22:01:43 +0000

    Read data chunks and prints to console

--- https_example.js
+++ https_example.js
@@ -7,7 +7,10 @@ var options = {
 
 var callback = function(response) {
   console.log('In response handler callback!');
-  console.log('Response: ', response);
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 }
 
 console.log("I'm about to make the request!");
